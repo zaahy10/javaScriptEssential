f45967d9302a103e53f63dcab08b1ded86d42000
Zaahy <zaahy10@gmail.com>
2026-01-30 07:46:36 -0500

    control flow lab

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,42 @@
+let userRole = "admin";
+let accessLevel;
+let isLoggedIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+if (userRole === "admin") {
+    accessLevel = "Full access granted";
+} else if (userRole === "manager") {
+    accessLevel = "Limited access granted";
+} else {
+    accessLevel = "No access granted";
+}
+
+if (isLoggedIn) {
+    if (userRole === "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+    }
+} else {
+    userMessage = "Please log in to access the system.";
+}
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Unknown";
+}
+
+console.log("Access Level:", accessLevel);
+console.log("User Message:", userMessage);
+console.log("User Category:", userCategory);
